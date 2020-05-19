1dc4ae5d8bcb06b43909c880b0909b351fa17043
Sama Winder <sgwinder@uw.edu>
2020-05-19 10:11:06 -0700

    Updated to reflect corrected precip values. There is now a significant positive relationship between tourism and precip

--- viz_model_clean.R
+++ viz_model_clean.R
@@ -19,7 +19,7 @@ modplot <- function(x){
 setwd("~/Documents/MAR/GIS/Predictors/Baseline_Inputs/")
 
 # read in the prepared predictors
-predictors <- read_csv("CombinedPredictors_20200320.csv")
+predictors <- read_csv("CombinedPredictors_20200519.csv")
 
 ## drop pids that are slivers
 slivers <- read_csv("../../AOI/AOI_v3/Intersected/slivers.csv")
@@ -30,7 +30,7 @@ slivers <- read_csv("../../AOI/AOI_v3/Intersected/slivers.csv")
 pred_small <- predictors %>%
   filter(!is.na(est_vis), !pid %in% slivers$pid) %>%
   dplyr::select(pid, vis_log, est_vis, Country, corals, mangroves, beach, forest, temp, dayshot,
-                precip, daysrain, protected, prop_land, wildlife, C3P,
+                precip, protected, prop_land, wildlife, C3P,
                 air_min_dist, ports_min_dist, pa_min_dist, ruins, sargassum, roads_min_dist,
                 prop_dev) 
 
@@ -41,7 +41,7 @@ pred_scaled <- pred_small %>%
   mutate(temp = scale_func(temp),
          dayshot = scale_func(dayshot),
          precip = scale_func(precip),
-         daysrain = scale_func(daysrain),
+         #daysrain = scale_func(daysrain),
          C3P = scale_func(C3P),
          air_min_dist = scale_func(air_min_dist),
          ports_min_dist = scale_func(ports_min_dist),
