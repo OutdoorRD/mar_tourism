64365968e3a46d29dbd4b9d87f331b2140381181
Sama Winder <sgwinder@uw.edu>
2021-02-18 13:33:43 -0800

    Generalized the spread_tourists script, and automated so that it can convert a whole folder in one go. Used the updated script to spread all tourists from the protect coral scenario

--- spread_tourists_across_ipms.R
+++ spread_tourists_across_ipms.R
@@ -4,21 +4,28 @@
 ##
 
 library(raster)
+library(stringr)
 
-setwd("~/Documents/MAR/")
+# choose location of files to convert
+setwd("~/Documents/MAR/ROOT/06_prot_corl/IPMs/")
+
+# list files to loop through
+files <- list.files("HexTourists")
 
 # read in an ipm, and also it's converter. let's do this for c1 and c2, just to look at values
 # TODO: teach it to pull out the relevant parts of the file name, and automatically write out the correct file names
+#file <- "bz_ipm_06_prot_corl_rec_clim0.tif"
+for(file in files){
+  ipm <- raster(paste0("HexTourists/", file))
+  
+  country <- str_extract(file, "[:alpha:]{2}")
+  converter <- raster(paste0("../../CountryAOIs/Conversions/", country, "_conversion.tif"))
+  
+  # convert
+  new <- ipm/converter
+  
+  # write it out
+  writeRaster(new, paste0("SpreadTourists/", file), format = "GTiff", datatype = "FLT4S", overwrite = TRUE)
+}
 
-ipm <- raster("ROOT/07_rest_corl/IPMs/bz_ipm_07_rest_corl_rec_clim2.tif")
-converter <- raster("ROOT/CountryAOIs/Conversions/bz_conversion.tif")
-
-new <- ipm/converter
-
-# is it really this easy?
-plot(new)
-
-# let's write it out
-writeRaster(new, "ROOT/07_rest_corl/IPMs/SpreadTourists/bz_ipm_07_rest_corl_rec_clim2_spread.tif", 
-            format = "GTiff", datatype = "FLT4S", overwrite = TRUE)
   
