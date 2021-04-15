4fa7258d9f12f9eb9dedc7938be1209db6052e12
Sama Winder <sgwinder@uw.edu>
2021-04-15 11:12:08 -0700

    Created a new script which removes any partial tourism hexes from the edges of the ROOT aoi before creating the raster IPMs. It's appropriate for any scenario, but has only been used to create the fire risk scenarios, since they are still in active progress and show obvious edge effect problems. The other scenarios are too far along to change (and the differences for them seem to be relatively minor)

---
+++ create_country_IPMs_MX_fire_risk.R
@@ -0,0 +1,114 @@
+##
+## This Version of create_country_IPMs is appropriate to be used for any scenario, but was developed
+## on 4/15/21 and only USED to create the fire risk ipms for MX. (The other scenarios are too far along
+## to be worth updating).
+## The difference is that it only creates IPM raster tourism values for hexes that fall entirely within
+## the ROOT AOI. This is a necessary patch because the "conversion" rasters accidentally give huge weight
+## to hex slivers that fall inside the Root AOI, assuming that those slivers are the total area across
+## which tourists should be spread, when, in fact, the actual hex extends beyond the root aoi.
+
+## Subsetting MARwide ipm geojsons into country-specific IPM rasters
+## 12/22/20
+## Forked from the bottom of `viz_predict_s4_restore_mangroves.R`
+##
+
+library(tidyverse)
+library(sf)
+library(lwgeom)
+library(raster)
+library(fasterize)
+
+setwd("~/Documents/MAR/")
+
+# read in MARwide IPM geojson
+marwide_path <- "ROOT/11_fire_mang/IPMs/MARwide_ipm_11_fire_mang_rec_clim0.geojson"
+modeled_sp <- read_sf(marwide_path)
+
+# pull some relevant pieces out
+anum <- str_extract(marwide_path, "[:digit:]{2}")
+aname <- str_extract(marwide_path, "(?<=[:digit:]{2}_)[:alpha:]+_[:alpha:]+")
+climate <- str_extract(marwide_path, "[:alnum:]*(?=.geojson)")
+ipm_dir <- str_extract(marwide_path, ".*(?=MARwide)")
+ipm <- paste0("ipm_", anum)
+
+#countries <- c("mx", "bz", "gt", "hn")
+#countries <- c("bz", "gt", "hn") # for watershed scenarios
+countries <- c("mx")
+#countries <- c("bz", "gt", "mx")
+#country <- "mx"
+
+for(country in countries){
+  # Set Country (protect mangrove gets all 4)
+  #country <- "gt"
+  #countryLong <- "Guatemala"
+  countryLong <- case_when(country == "mx" ~ "Mexico",
+                           country == "bz" ~ "Belize",
+                           country == "gt" ~ "Guatemala",
+                           country == "hn" ~ "Honduras")
+  
+  print(paste("Beginning to process", countryLong))
+  
+  # import empty country raster & country aoi outline
+  country_rast <- raster(paste0("ROOT/CountryAOIs/", country, "_root_aoi.tif"))
+  country_sf <- read_sf(paste0("ROOT/CountryAOIs/", country, "_root_aoi.shp"))
+  
+  
+  modeled_country <- modeled_sp %>% filter(CNTRY_NAME == countryLong)
+  
+  # check it out
+  fig <- ggplot(modeled_country) +
+    geom_sf(aes(fill = diff_vis))
+  
+  print(fig)
+  
+  modeled_country
+  summary(modeled_country)
+  
+  ## Extract the modeled estimates and difference and write it out
+  country_clean <- modeled_country %>%
+    dplyr::select(pid, CNTRY_NAME, est_vis, preds_base_clim_vis_future, preds_scen_clim_vis_future, diff_vis, perc_change)
+  
+  # write out shp
+  st_write(country_clean, paste0(ipm_dir, country, "_", ipm, "_", aname, "_rec_", climate, ".geojson"))#, delete_dsn = TRUE)
+  
+  
+  
+  ### Convert to Raster 
+  
+  # raster type to float 32 is what matthew thinks will help
+  # transform
+  country_tran <- st_transform(country_clean, crs = 26916) 
+  
+  # clip to only include hexes which are completely inside the root AOI (to avoid edge effects), added 4/15/21
+  inside <- st_covered_by(country_tran, country_sf)
+  country_inside <- country_tran[lengths(inside) > 0, ]
+  ggplot(country_inside) + geom_sf()
+  
+  
+  diff_rast <- fasterize(country_inside, country_rast, field = "diff_vis", background = 0)
+  diff_rast
+  summary(country_clean)
+  #plot(diff_rast)
+  #dataType(diff_rast) <- "FLT4S"
+  #dataType(diff_rast)
+  
+  # mask it
+  # NOTE: if this throws strange errors/warnings, go to `tmp/R******` and delete all the temp rasters that have been created
+  diff_masked <- mask(diff_rast, mask = country_sf, datatype = "FLT4S")
+  diff_masked
+  
+  # spread tourists according to conversion factor (new 2/19/21)
+  converter <- raster(paste0("ROOT/CountryAOIs/Conversions/", country, "_conversion.tif"))
+  
+  # convert
+  diff_spread <- diff_masked/converter
+  
+  # write it out
+  writeRaster(diff_spread, 
+              paste0(ipm_dir, country, "_", ipm,"_", aname, "_rec_", climate, ".tif"), 
+              format = "GTiff", datatype = "FLT4S", overwrite = TRUE)
+  
+  # remove temp files
+  files <- list.files(tempdir(), full.names = TRUE)
+  unlink(files, recursive = TRUE)
+}
